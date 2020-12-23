1c586db06aa8d63d09f9c4501cd14a92a80db5c3
Cheol_Min <ho9273@naver.com>
2020-12-23 19:02:09 +0900

    일부 수정
    1. 일부 함수명 수정
    2. 마법 발사시 방향을 재조정하도록 수정

--- Assets/Scripts/DestoryEffect.cs
+++ Assets/Scripts/DestoryEffect.cs
@@ -1,19 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿/** 
+ * 범위를 나가면 이펙트를 파괴시키는 스크립트
+ */
+
 using UnityEngine;
 
 public class DestoryEffect : MonoBehaviour
 {
-    private Magic magic;
+    private Magic _magic;
+
     private void FixedUpdate()
     {
         if (transform.position.z < -2 || transform.position.z > 15
             || transform.position.x < -7 || transform.position.x > 7
             || transform.position.y < -1 || transform.position.y > 10)
         {
-            magic.UpdatePercent();
+            _magic.UpdatePercent();
             Destroy(gameObject);
         }
     }
-    public void SetMagic(Magic magic) => this.magic = magic;
+
+    public void SetMagic(Magic magic) => this._magic = magic;
 }
